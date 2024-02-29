6928eafd804e187dd80807de53db39360e472630
Filip <filip.uradnik9@gmail.com>
2024-02-29 23:16:11 +0100

    Update rust name.

--- rust_crates.sh
+++ rust_crates.sh
@@ -3,10 +3,10 @@
 cargo install cargo-docset
 mkdir tmp
 cd tmp
-git clone ${1} cloned
-cd cloned
+git clone ${1} $2
+cd $2
 cargo doc --no-deps -p $2
-cargo docset
+cargo docset --docset-name $2
 cp -r target/docset/*.docset ../../build
 cd ../..
 rm -rf tmp
