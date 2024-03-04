3738e13eca034a94e769c546ab1e94c30401195e
Filip <filip.uradnik9@gmail.com>
2024-03-04 14:38:58 +0100

    Fix straka unzip

--- straka.sh
+++ straka.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
 mkdir tmp
+mkdir -p build
 cd tmp
 curl -L https://redmine.ms.mff.cuni.cz/projects/straka/repository/straka-tf-zeal-docs/revisions/master/raw/docsets/${1}.docset.tar > user.tgz
-tar -xzf user.tgz
+tar -xf user.tgz
 rm user.tgz
-mv *.docset ../build
+mv *.docset ../build/
 cd ..
 rm -rf tmp
 
