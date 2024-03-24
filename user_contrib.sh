5b95f91d58e1d9d306848912cf2b4cb6f61a9fbe
Filip <filip.uradnik9@gmail.com>
2024-03-24 23:14:19 +0100

    fix: properly copy out built datasets when first

--- user_contrib.sh
+++ user_contrib.sh
@@ -3,6 +3,7 @@ cd tmp
 curl -L london.kapeli.com/feeds/zzz/user_contributed/build/${1}/${2}.tgz > user.tgz
 tar -xzf user.tgz
 rm user.tgz
+mkdir -p ../build
 mv *.docset ../build
 cd ..
 rm -rf tmp
